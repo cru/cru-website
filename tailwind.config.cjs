9ed31b1f86b268f9c2ed8aab2c003c746fbd58f5
Brandon Lee <38223263+sierra403@users.noreply.github.com>
2024-09-04 15:19:36 -0600

    configure theme as UC red

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,8 +10,19 @@ module.exports = {
         display: 'Ubuntu, ui-serif', // Adds a new `font-display` class
       },
       colors: {
-        'brand-primary': colors.violet,
-        'brand-secondary': colors.fuchsia,
+        'brand-primary': {
+          50: colors.red[50],
+          100: colors.red[100],
+          200: colors.red[200],
+          300: colors.red[300],
+          400: colors.red[400],
+          500: '#d6001c',
+          600: colors.red[600],
+          700: colors.red[700],
+          800: colors.red[800],
+          900: colors.red[900],
+        },
+        'brand-secondary': colors.amber,
       },
     },
   },
