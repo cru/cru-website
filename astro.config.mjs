cfdc2bdbaa96a370da9f1c858ccef66df3f9852f
Brandon Lee <38223263+sierra403@users.noreply.github.com>
2024-09-24 14:02:15 -0600

    redirect rule for old pricing link due to distribution of the old link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  redirects: {
+    '/services-and-pricing/edc-services': '/data-capture/pricing',
+  },
 })
