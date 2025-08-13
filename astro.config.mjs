03d8efe8d1d0f03f16ae435b2109a249e68e2edd
Brandon Lee <38223263+sierra403@users.noreply.github.com>
2025-08-13 15:43:05 -0600

    migrate tailwind from v3 to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from "@tailwindcss/vite";
 import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   redirects: {
     '/services-and-pricing/edc-services': '/data-capture/pricing',
   },
