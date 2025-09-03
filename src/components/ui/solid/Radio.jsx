689d2781172f7f298fbfa9ecfbe2665cf4d33272
sierra403 <sierra403@users.noreply.github.com>
2025-09-03 20:24:46 +0000

    Apply formatting changes

--- src/components/ui/solid/Radio.jsx
+++ src/components/ui/solid/Radio.jsx
@@ -1,16 +1,14 @@
-import "@awesome.me/webawesome/dist/components/radio-group/radio-group.js"
-import { Index } from "solid-js"
+import '@awesome.me/webawesome/dist/components/radio-group/radio-group.js'
+import { Index } from 'solid-js'
 
 const Radio = (props) => {
-	return (
-		<wa-radio-group size="small" {...props} class={`${props.classes}`}>
-			<Index each={props.options}>
-				{(option) => (
-					<wa-radio value={option().value}>{option().label}</wa-radio>
-				)}
-			</Index>
-		</wa-radio-group>
-	)
+  return (
+    <wa-radio-group size="small" {...props} class={`${props.classes}`}>
+      <Index each={props.options}>
+        {(option) => <wa-radio value={option().value}>{option().label}</wa-radio>}
+      </Index>
+    </wa-radio-group>
+  )
 }
 
 export default Radio
--- src/components/ui/solid/TextArea.jsx
+++ src/components/ui/solid/TextArea.jsx
@@ -1,14 +1,14 @@
-import "@awesome.me/webawesome/dist/components/textarea/textarea.js"
+import '@awesome.me/webawesome/dist/components/textarea/textarea.js'
 
 const TextArea = (props) => {
-	return (
-		<wa-textarea
-			appearance="filled"
-			size="small"
-			{...props}
-			class={`${props.classes}`}
-		></wa-textarea>
-	)
+  return (
+    <wa-textarea
+      appearance="filled"
+      size="small"
+      {...props}
+      class={`${props.classes}`}
+    ></wa-textarea>
+  )
 }
 
 export default TextArea
