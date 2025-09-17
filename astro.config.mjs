76ee35474b559e72c2f37fc0e282cba9876e9ff1
Brandon Lee <38223263+sierra403@users.noreply.github.com>
2025-09-17 11:54:30 -0600

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,19 @@ import { defineConfig } from 'astro/config'
 
 import solidJs from '@astrojs/solid-js'
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), solidJs()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   redirects: {
     '/services-and-pricing/edc-services': '/data-capture/pricing',
   },
+
+  adapter: netlify(),
 })
