60340c4ef1317bf65024e14522e2df412f7d1d3e
Brandon Lee <38223263+sierra403@users.noreply.github.com>
2025-09-17 11:21:36 -0600

    Revert "add node adapter for server-side actions"
    This reverts commit 5e1db6c23747425841e3939509f4d865d58c26b4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,13 @@ import mdx from '@astrojs/mdx'
 
 import solidJs from '@astrojs/solid-js'
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), solidJs()],
-
   vite: {
     plugins: [tailwindcss()],
   },
-
   redirects: {
     '/services-and-pricing/edc-services': '/data-capture/pricing',
   },
-
-  adapter: node({
-    mode: 'standalone',
-  }),
 })
