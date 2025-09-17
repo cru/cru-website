2c6830eb5ebd033315e38f091949d437b1c26950
sierra403 <sierra403@users.noreply.github.com>
2025-09-17 18:15:58 +0000

    Apply formatting changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config'
 
 import solidJs from '@astrojs/solid-js'
 
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
