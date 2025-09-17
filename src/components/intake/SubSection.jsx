dbeeefc9a09ba60680a33312ae178054a5384cdb
Brandon Lee <38223263+sierra403@users.noreply.github.com>
2025-09-17 12:45:45 -0600

    put heading back onto form step

--- src/components/intake/SubSection.jsx
+++ src/components/intake/SubSection.jsx
@@ -11,10 +11,11 @@ const SubSection = (props) => {
     <section
       class={`space-y-8 overflow-hidden p-1 transition-all duration-700 ease-in-out ${
         props.hidden
-          ? 'pointer-events-none absolute bottom-0 h-0 -translate-x-64 scale-80 opacity-0'
-          : 'max-h-screen translate-x-0 translate-y-0 scale-100 opacity-100'
+          ? 'pointer-events-none absolute bottom-0 h-0 opacity-0'
+          : 'max-h-screen opacity-100'
       }`}
     >
+      <h4>{props.title}</h4>
       <form ref={(el) => (formRef = el)}>{props.children}</form>
     </section>
   )
