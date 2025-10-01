5f4ac1046b171d6cca038889be0bcdc38f2eb75b
Brandon Lee <38223263+sierra403@users.noreply.github.com>
2025-10-01 16:10:31 -0600

    enable cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,21 @@
-import mdx from '@astrojs/mdx'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'astro/config'
-
-import solidJs from '@astrojs/solid-js'
-
-import netlify from '@astrojs/netlify'
+import cloudflare from '@astrojs/cloudflare';
+import mdx from '@astrojs/mdx';
+import solidJs from '@astrojs/solid-js';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   integrations: [mdx(), solidJs()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   redirects: {
     '/services-and-pricing/edc-services': '/data-capture/pricing',
   },
-  adapter: netlify({
-    imageCDN: false,
-  }),
+
+  adapter: cloudflare(),
 })
