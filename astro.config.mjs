dc830baee08d0ade77c6560065455c68558b289a
sierra403 <sierra403@users.noreply.github.com>
2025-10-01 22:11:05 +0000

    Apply formatting changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import cloudflare from '@astrojs/cloudflare';
-import mdx from '@astrojs/mdx';
-import solidJs from '@astrojs/solid-js';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare'
+import mdx from '@astrojs/mdx'
+import solidJs from '@astrojs/solid-js'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
